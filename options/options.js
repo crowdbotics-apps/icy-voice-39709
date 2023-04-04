70fe25d28aa03f40d46ffb34101e10ccd0d2d290
Crowdbotics <team@crowdbotics.com>
2023-04-04 18:31:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "icy-voice-39709",
+  url: "https://icy-voice-39709.botics.co",
+  api: "https://icy-voice-39709.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
